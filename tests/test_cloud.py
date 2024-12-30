9530af875ab4f74bec961d515747bc9a60bf584d
Rachel House <rachel.house@greatexpectations.io>
2024-12-30 16:00:30 -0600

    finish test for cloud cred check

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -7,12 +7,19 @@ import tutorial_code as tutorial
 def test_check_for_gx_cloud_credentials_exist(monkeypatch):
     """Test that function raises an error when credentials are not found."""
 
-    # monkeypatch.delenv("GX_CLOUD_ORGANIZATION_ID", raising=False)
-    # monkeypatch.delenv("GX_CLOUD_ACCESS_TOKEN", raising=False)
-
     with pytest.raises(
         ValueError, match=r"GX_CLOUD_ORGANIZATION_ID environment variable is undefined"
     ):
         tutorial.cloud.check_for_gx_cloud_credentials_exist()
 
-    # monkeypatch.setenv("API_AUDIENCE", "https://mock.com")
+    monkeypatch.setenv("GX_CLOUD_ORGANIZATION_ID", "<test-org-id>")
+
+    with pytest.raises(
+        ValueError, match=r"GX_CLOUD_ACCESS_TOKEN environment variable is undefined"
+    ):
+        tutorial.cloud.check_for_gx_cloud_credentials_exist()
+
+    monkeypatch.setenv("GX_CLOUD_ACCESS_TOKEN", "<test-access-token>")
+
+    # GX Cloud credentials are found.
+    tutorial.cloud.check_for_gx_cloud_credentials_exist()
